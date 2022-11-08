93db9063edf784e3c263f6fcd98c05abfaa22996
warasil <simon.perdriaud@gmail.com>
2022-11-08 17:47:43 +0100

    its ok

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
 
-const port = 5000;
+const port = 9001;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -11,9 +13,12 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
+const usersHandlers = require("./usersHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users", usersHandlers.getUsers);
+app.get("/api/users/:id", usersHandlers.getUserById);
 
 app.listen(port, (err) => {
   if (err) {
---
+++ server.js
@@ -0,0 +1,1 @@
+const port = process.env.APP_PORT ?? 9001;
