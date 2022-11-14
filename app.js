acc4086f1d389e533076bad54f411ccc3ca37735
warasil <simon.perdriaud@gmail.com>
2022-11-14 11:29:41 +0100

    express3

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
-
 const express = require("express");
 
 const app = express();
+
+app.use(express.json());
 
 const port = 9001;
 
@@ -19,6 +20,8 @@ app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", usersHandlers.getUsers);
 app.get("/api/users/:id", usersHandlers.getUserById);
+app.post("/api/movies", movieHandlers.postMovie);
+app.post("/api/users", usersHandlers.postUser);
 
 app.listen(port, (err) => {
   if (err) {
